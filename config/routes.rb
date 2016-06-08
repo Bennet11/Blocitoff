0c12b89df179706758b451e0dbb3909cb85dd8de
Bennet11 <bennetveloya@gmail.com>
2016-06-08 14:26:30 -0700

    creating to do items

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/show'
+
 
   devise_for :users
-  get 'welcome/index'
 
-  get 'welcome/about'
+  resources :users, only: [:show] do
+    resources :items, only: [:create]
+  end
 
   root to: 'users#show'
+
+  get 'users/show'
+  get 'welcome/index'
+  get 'welcome/about'
 end
