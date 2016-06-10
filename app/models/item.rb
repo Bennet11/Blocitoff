a8b4bf1a1851ba49457141c67e76522f59d2a074
Bennet11 <bennetveloya@gmail.com>
2016-06-10 02:00:59 -0700

    failed to delete expired items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ActiveRecord::Base
   belongs_to :user
 
-  default_scope { order('updated_at DESC') }
+  default_scope { order('updated_at ASC') }
 
   def days_left
-  7 - (DateTime.now.utc.to_date - created_at.to_date).to_i
+   -1 - (DateTime.now.utc.to_date - created_at.to_date).to_i
   end
 end
