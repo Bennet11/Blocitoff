8fc36de20bcbd12fa96ea50bcc4ff5d20ab72121
Bennet11 <bennetveloya@gmail.com>
2016-06-10 16:34:15 -0700

    expired todo lists still not working

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,10 @@ class Item < ActiveRecord::Base
   default_scope { order('updated_at ASC') }
 
   def days_left
-   -1 - (DateTime.now.utc.to_date - created_at.to_date).to_i
+   7 - (DateTime.now.to_date - created_at.to_date).to_i
+  end
+
+  def delete_expire_items
+    delete if days_left < 0
   end
 end
